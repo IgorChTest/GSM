613e87194ed4d6d061da1f4e4735c8bb3e2419a9
IgorCH QA <ic641716@gmail.com>
2023-12-25 14:43:30 +0300

    Changed commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 100; //Начальная сумма на счету
-        int y = 1100; //Сумма пополнения
-        int z = 100; //Сумма пополнения для начисления 1 руб. бонуса
-        int x = (y / z); //Сумма начисленных бонусов
-        if (y > 1000) {
-            System.out.println(a + y + x);
+        int startAmount = 100; //Начальная сумма на счету
+        int addAmount = 1100; //Сумма пополнения
+        int bonusBase = 100; //Сумма пополнения для начисления 1 руб. бонуса
+        int bonus = (addAmount / bonusBase); //Сумма начисленных бонусов
+        if (addAmount > 1000) {
+            System.out.println("Получено бонусов " + bonus);
+            System.out.println("Баланс счета " + (startAmount + addAmount + bonus));
         } else {
-            System.out.println(a + y);
+            System.out.println("Баланс счета " + startAmount + addAmount);
         }
     }
 }
